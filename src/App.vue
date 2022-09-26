501faea8b7403fa2db9143df944cb9c2f09f206b
Estiven Reyes <estivenreyes2013@hotmail.com>
2022-09-26 01:42:58 -0500

    forums sections ready

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
-
+  import Mainforum from './components/Mainforum.vue'
 </script>
 
 <template>
   <div class="page">
-      <header>
+    <header>
       <div class="nav">
-        <img alt="Parrot logo" class="logo" src="./assets/img/logo.svg"/>
+        <img alt="Parrot logo" class="logo" src="./assets/img/logo.svg" />
         <nav class="navbar" id="navegation menu">
         </nav>
-      </div>   
+      </div>
     </header>
 
     <section class="landing">
@@ -19,7 +19,7 @@
       <div class="landing__content">
         <h2 class="landing--h2">Welcome to</h2>
         <h1 class="landing--h1">Parrot Games</h1>
-        <p class="landing--text">Parrot Games is a video game distribution company with a social component, 
+        <p class="landing--text">Parrot Games is a video game distribution company with a social component,
           where our users can interact with others, criticize and review their favorite video games.</p>
       </div>
     </section>
@@ -40,26 +40,33 @@
         </figure>
       </div>
     </section>
+
+    <!--slider here-->
+    <!-- <section class="games">
+      <h2 class="game--text">Explore a long list of your favorite games</h2>
+    </section> -->
+    <main>
+      <Mainforum/>
+    </main>
   </div>
-  <main>
-    <TheWelcome />
-  </main>
+
+  <footer></footer>
 </template>
 
 <style lang="scss">
-  $blue: #1569DB;
-  $gray: #2E2E2E;
-  $nav: #232323;
+$blue: #1569DB;
+$gray: #2E2E2E;
+$nav: #232323;
 
-*{
+* {
   box-sizing: border-box;
 }
 
-body{
+body {
   margin: 0;
 }
 
-.page{
+.page {
   width: 100%;
   font-family: satoshi;
 }
@@ -71,13 +78,13 @@ body{
   justify-content: space-between;
   position: fixed;
   background: rgba(35, 35, 35, 0.8);
-  
+
   padding: 23px;
   color: #000000;
   z-index: 10;
 }
 
-.landing{
+.landing {
   background-image: url("../src/assets/img/landingPage.png");
   background-size: cover;
   background-repeat: no-repeat;
@@ -86,50 +93,50 @@ body{
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  padding: 485px 195px;
+  padding: 390px 195px;
   justify-content: center;
   margin: 0 auto;
   font-family: satoshi;
 
-  &__content{
+  &__content {
     width: 40%;
     color: white;
   }
 
-  &--svg{
+  &--svg {
     width: 40%;
     padding-block-end: 10px;
     display: flex;
     justify-content: center;
   }
 
-  &--logo{
+  &--logo {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    
+
 
   }
 
-  &--h1{
+  &--h1 {
     display: flex;
     justify-content: center;
     font-size: 4em;
     font-weight: bold;
   }
 
-  &--h2{
+  &--h2 {
     display: flex;
     justify-content: center;
     font-size: 1.9em;
   }
 
-  &--text{
+  &--text {
     display: flex;
     text-align: center;
     font-size: 1.3em;
-    padding: 10px 50px;
+    padding: 10px 27px;
   }
 }
 
@@ -139,28 +146,30 @@ body{
   flex-wrap: wrap;
   width: 100%;
   justify-content: space-around;
-  padding: 100px; 
+  padding: 100px;
 
-  &__container{
+  &__container {
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
     justify-content: center;
     width: 100%;
     margin: 100px;
-    
+
   }
 
-  &--shop{
+  &--shop {
     width: 25%;
     text-align: center;
     margin: 50px;
   }
 
-  &--text{
-    padding: 30px;
+  &--text {
+    padding: 20px;
     font-size: 1.3em;
   }
 }
 
+// Samsung galaxy S8
+@media only screen and (min-width:768px) {}
 </style>
